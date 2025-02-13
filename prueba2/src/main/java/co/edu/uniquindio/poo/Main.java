d54d9571ed0e659f9d00921cb4b387609eb20444
isa5022 <isabelf.angulom@uqvirtual.edu.co>
2025-02-12 22:42:28 -0500

    inicio

---
+++ prueba2/src/main/java/co/edu/uniquindio/poo/Main.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int edad = 40;
+    double altura = 1.80;
+    String name = "miguel";
+    System.out.println( "nombre:"+ name);
+    System.out.println( "edad:"+ edad);
+    System.out.println( "altura:"+ altura);
+
+
+
+        }
+    }
