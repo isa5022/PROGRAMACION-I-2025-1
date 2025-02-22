8a67bc1e7803bb7f7f592283b084a0c7d99fb391
isa5022 <isabelf.angulom@uqvirtual.edu.co>
2025-02-22 10:26:24 -0500

    guarderia, empleados

--- prueba/src/main/java/co/edu/uniquindio/poo/Main.java
+++ prueba/src/main/java/co/edu/uniquindio/poo/Main.java
@@ -2,16 +2,21 @@ package co.edu.uniquindio.poo;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int edad = 40;
+        double altura = 1.80;
+        String name = "isa";  // Corrección: String en lugar de sting
+        System.out.println("nombre: " + name);
+        System.out.println("edad: " + edad);
+        System.out.println("altura: " + altura);
     }
 }
+
+
+
+
+
+
+
